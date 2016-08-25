0ef856ac132662ac040714574e17660ccae0a6da
Christian Fobel <christian@fobel.net>
2016-08-25 13:07:14 -0400

    Add option to exclude file(s)

--- rename_package_files/__init__.py
+++ rename_package_files/__init__.py
@@ -1,9 +1,15 @@
+import fnmatch
+import logging
+
 from path_helpers import path
 # TODO Remove dependency on `pandas`
 import pandas as pd
 
 
-def rename_package_files(package_dir, old_name, new_name):
+logger = logging.getLogger(__name__)
+
+
+def rename_package_files(package_dir, old_name, new_name, exclude=None):
     '''
     Rename all occurrences of ``old_name`` to ``new_name`` in all files in
     package.
@@ -26,6 +32,13 @@ def rename_package_files(package_dir, old_name, new_name):
     new_name
         New (``-``-separated) package name
     '''
+    if exclude is None:
+        exclude = lambda old_name_i: False
+    elif not callable(exclude):
+        exclude_list = [exclude] if isinstance(exclude, str) else exclude
+        exclude = lambda old_name_i: any(fnmatch.fnmatch(old_name_i, exclude_i)
+                                         for exclude_i in exclude_list)
+
     names = pd.Series([old_name, new_name], index=['old', 'new'])
     underscore_names = names.map(lambda v: v.replace('-', '_'))
     camel_names = names.str.split('-').map(lambda x: ''.join([y.title()
@@ -37,10 +50,14 @@ def rename_package_files(package_dir, old_name, new_name):
     # Dashes are used in Python package names, but underscores are used in
     # Python module names.
     for p in path(package_dir).walkfiles():
+        if exclude(p):
+            logger.debug('.. skipping contents rename of %s', p)
+            continue
         data = p.bytes()
         if '.git' not in p and (names.old in data or
                                 underscore_names.old in data or
                                 camel_names.old in data):
+            logger.debug('.. rename contents of %s', p)
             p.write_bytes(data.replace(names.old, names.new)
                           .replace(underscore_names.old, underscore_names.new)
                           .replace(camel_names.old, camel_names.new))
@@ -64,4 +81,8 @@ def rename_package_files(package_dir, old_name, new_name):
         rename_path(p)
 
     for p in path(package_dir).walkfiles():
-        rename_path(p)
+        if exclude(p):
+            logger.debug('.. skipping rename of %s', p)
+        else:
+            logger.debug('.. rename %s', p)
+            rename_path(p)
